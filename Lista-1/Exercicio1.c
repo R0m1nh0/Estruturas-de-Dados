a45bccc8f2c7fa441f568dc1610de993e43cc111
R0m1nh0 <romualdoosfilho@hotmail.com>
2014-09-09 15:56:10 -0300

    Update Exercicio1.c

--- Lista-1/Exercicio1.c
+++ Lista-1/Exercicio1.c
@@ -1,5 +1,34 @@
 #include <stdio.h>
 
-int main (void)
-{
+int main(void){
+
+  int num1 = 0;
+  int num2 = 0;
+
+  printf("digite o primeiro numero");
+  scanf("%i", &num1);
+
+  printf("digite o segundo numero");
+  scanf("%i", &num2);
+
+  int *pnum1 = &num1;
+  int *pnum2 = &num2;
+
+  void trocarValores(int *x, int *y);
+
+  trocarValores(pnum1, pnum2);
+
+  printf("numero 1 = %i\nnumero 2 = %i\n",num1,num2);
+
+  return 0;
 }
+
+void trocarValores(int *x, int *y){
+
+  int aux = 0;
+  aux = *x;
+  *x = *y;
+  *y = aux;
+
+}
+
